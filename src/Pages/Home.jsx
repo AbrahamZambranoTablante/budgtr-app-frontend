5952787276cf2670fa4fc6cc71a832e4fc3d5b1d
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-07-09 18:24:05 -0400

    fixing media queries for home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,9 +4,9 @@ export default function Home () {
     return(
         <>
             <div className="home-container">
-                <h1>Budgetmin</h1>
+                <h1 className="app-title">Budgetmin</h1>
                 <p>"Master Your Money, Simplify Your Life"</p>
-                <img src="/benjamin.png" alt="golden coin" width="300" height="300"/>
+                <img src="/benjamin.png" alt="golden coin" width="300" height="340"/>
                 <Link to={"/transactions"}>
                     <button>Start</button>
                 </Link>
